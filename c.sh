20ec16ea09384f17b5442d855bc6c5229dc527e1
BloodVision <67549467+BloodVision@users.noreply.github.com>
2020-07-11 10:37:27 +0530

    Add files via upload

---
+++ c.sh
@@ -0,0 +1,36 @@
+igdsitditdtiditditsdtidtditditditsitsurditdursit
+
+
+
+
+madarcgod bhadvrme randi ki aulaad itna pelunga na ki maa chud jyegi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+cp /data/user/0/com.AndLua.LY/app_lua/import.lua /storage/emulated/0/tmp/
+g,yducgicgu ig  ig vi ic igxigcgocufchov
+gxicig o oh ho igvohciy
+vijvohcgicohocouvljc
+ ohvln bkxihchocigcojcin
